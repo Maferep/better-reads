532a3b0dc150c9d868b4a1f451732e50b0df9e38
Mariano Lazzarini <marianolazzarini01@gmail.com>
2024-10-28 20:04:35 -0300

    Checkeo para evitar que usuario pueda estar logeado a la pagina por session, si no existe tal usuario

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,5 +1,17 @@
+import Database from 'better-sqlite3';
+
 // middleware to test if authenticated
 export function isAuthenticated (req, res, next) {
-  if (req.session.user) next()
+  console.log("Checking authentication", req.session)
+  if (req.session.user &&
+      req.session.userId &&
+      estaEnBaseDeDatosUsuarios(req.session.userId, req.session.user)) next()
     else next('route')
+}
+
+function estaEnBaseDeDatosUsuarios(userId, username) {
+  const db = new Database('database_files/betterreads.db', { verbose: console.log });
+  const check = db.prepare('SELECT 1 FROM insecure_users WHERE id = ? AND username = ?');
+  const rows = check.all(userId, username);
+  return rows.length > 0;
 }
