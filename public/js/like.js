6867460814236a45ff5c59aef6cfe028d5a898eb
Maferep <mf.pont.tv@gmail.com>
2024-11-03 13:46:39 -0300

    refctor like to external file

--- public/js/like.js
+++ public/js/like.js
@@ -1,5 +1,8 @@
+var this_js_script = document.currentScript; // or better regexp to get the file name..
 
-document.getElementById("container").addEventListener("click", function(event) {
+var event_listener_location = this_js_script.getAttribute("event_listener_location");  
+
+document.getElementById(event_listener_location).addEventListener("click", function(event) {
   if (event.target.closest(".like-button")) {
     event.preventDefault();
     const postDiv = event.target.closest(".post");
@@ -29,3 +32,22 @@ document.getElementById("container").addEventListener("click", function(event) {
     })
   }
 }); 
+
+//Actualiza el contador de likes en la vista de un post
+async function updateLikes() {
+  const post = document.getElementsByClassName("post")[0]
+  const post_id = post.getAttribute("post-id");
+
+  let response = await fetch(`http://localhost/post/${post_id}/like`, {
+    method: "GET",
+  });
+  let data = await response.json();
+
+  const heartToUse= data.liked ? "❤️" : "🤍";
+  post.querySelector(".like-counter").textContent = `${heartToUse} ${data.like_count}`;
+}
+
+//Actualiza el contador de likes en la vista de un post cuando se muestra la pagina
+window.addEventListener('pageshow', function(event) {
+    updateLikes();
+});
