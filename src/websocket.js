e54c064153cb4d9d2ee3a83c4f87560cc39f4f4d
Danikim01 <dakim@fi.uba.ar>
2024-11-17 18:06:27 -0300

    delete book from cart notification added

--- src/websocket.js
+++ src/websocket.js
@@ -1,4 +1,4 @@
-import { addBookToCart,removeFromCart } from "./database.js";
+import { addBookToCart,removeFromCart,retrieveFromCart,fetchBook } from "./database.js";
 
 export default io => {
     io.on('connection', socket => {
@@ -9,9 +9,24 @@ export default io => {
         });
 
         socket.on("removeFromCart",(data) => {
-            console.log("Book removed from cart: ", data);
             removeFromCart(data.userId, data.bookId);
-            socket.emit("bookRemovedFromCart");
+            const remaining_books = retrieveFromCart(data.userId);
+            if (remaining_books.length !== 0) {
+                let books = remaining_books.map((book) => {
+                    return fetchBook(book.book_id);
+                });
+                //format the author and the genre of the book, removing the [] and the "".
+                books = books.map((book) => {
+                    book.authors = book.authors.replace(/[\[\]"]+/g, '');
+                    book.genre = book.genre.replace(/[\[\]"]+/g, '');
+                    book.userId = data.userId;
+                    return book;
+                });
+                console.log("remaining books", books);
+                socket.emit("bookRemovedFromCart",books);
+            }else{
+                socket.emit("bookRemovedFromCart",remaining_books);
+            }
         });
     });
 }
